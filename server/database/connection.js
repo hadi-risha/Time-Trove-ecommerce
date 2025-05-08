8e09e6f9ad1d8e6c1ce46fa36a23a2b3c118b66e
Hadi <hadiyakhadija62@gmail.com>
2025-05-08 16:04:27 +0530

    some changes made

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => { 
     try {
-        const con = await mongoose.connect(process.env.MONGO_URI)
+        const con = await mongoose.connect(process.env.MONGO_URL);
         console.log(`MongoDB connected ${con.connection.host}`)
     } catch (err) {
         console.log(err);
